841b50df2e8d8e15aeae49141fe97bfe1a662e9c
Suykum <suikum2006@gmail.com>
2019-09-05 10:59:16 +0300

    Strategy

---
+++ src/main/java/strategy/MallardDuck.java
@@ -0,0 +1,13 @@
+package strategy;
+
+public class MallardDuck extends Duck {
+    public MallardDuck() {
+        quackBehavior = new Quack();
+        flyBehavior = new FlyWithWing();
+    }
+
+    @Override
+    public void display() {
+        System.out.println("I'm real Mallard duck");
+    }
+}
